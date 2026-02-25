c1627c6d7085ad15b555cca4db0a2bdb9d4d265a
Manal Belhadj <mbelhadj@evicertia.com>
2026-02-25 18:10:28 +0100

    cambios finales, api operativa

---
+++ Server.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Server.Services;
+
+namespace Server.Tests
+{
+    class CalculatorServiceTests
+    {
+
+        [Test]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            var calculatorService = new CalculatorServiceImp();
+            int a = 5;
+            int b = 10;
+            // Act
+            int result = calculatorService.Add(a, b);
+            // Assert
+            Assert.AreEqual(15, result);
+		}
+	}
+}
